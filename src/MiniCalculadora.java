91c11c70021bd8f6c4158dcb44a0d33eba8e406b
Brian <briancortesperezcejuela@gmail.com>
2026-04-09 10:14:03 +0200

    Valor-absoluto

---
+++ src/MiniCalculadora.java
@@ -0,0 +1,10 @@
+public class MiniCalculadora {
+
+        /**
+         * Método que recibe un número double y devuelve su valor absoluto.
+         */
+        public double valorAbsoluto(double numero) {
+            return Math.abs(numero);
+        }
+
+}
